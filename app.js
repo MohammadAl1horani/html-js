e0431fafa352cab3b82b775b47ef74e981563bcc
MohammedAlhoran <>
2021-06-02 14:01:38 +0000

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+alert('welcome')
+var travel = prompt('do you like to travel')
+document.write('<p>'+"do you like traveling? "+travel+'</p>')
+if(travel=='yes'){
+  document.write('<h2>'+"CALL US"+'</h2>'), prompt('add your phone number')
+}
+
+
+// i wanted to save the phone number but i couldn't :'(
